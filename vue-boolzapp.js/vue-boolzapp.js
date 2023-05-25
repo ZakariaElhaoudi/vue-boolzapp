46a0232b6eefb9d2069d96fbfc98ee8cd078d89e
Zakaria96 <zakarialavoro@gmail.com>
2023-05-25 15:30:40 +0200

    update dropdown and deleteMessage

--- vue-boolzapp.js/vue-boolzapp.js
+++ vue-boolzapp.js/vue-boolzapp.js
@@ -10,7 +10,10 @@ Milestone 3
 Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 Milestone 4
-Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)*/
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+*/
 
 const currentDate = new Date();
 const day = String(currentDate.getDate()).padStart(2, '0');
@@ -229,13 +232,13 @@ createApp({
             },1000);
         },
         deleteMessage(index) {
-            this.contacts[this.currentChat].splice(index,1)
+            this.contacts[this.currentChat].messages.splice(index,1)
         },
         
-        toggle () {
-            this.active = !this.active
-        }
-          
+        toggleDropdown(index) {
+            this.contacts[this.currentChat].messages[index].active = !this.contacts[this.currentChat].messages[index].active;
+            
+        },
     }
 }).mount('#container')
 
